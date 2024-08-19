f5e416cda800d82a6a9801449f4cb793f63f516c
Muhammad Gema Rhomadhon <106788957+gema321@users.noreply.github.com>
2024-08-19 08:22:21 +0700

    Push Hello

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Worldaaaaaaaa!");
+Console.WriteLine("Hello, Worldaaaaaaaa!");
+Console.WriteLine("Hello, Worldaaaaaaaa!");
